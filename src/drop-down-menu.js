879806ca6ff56d0f11222022d6f28af120c2acf1
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2023-01-21 17:18:48 +0000

    Apply active class to container over menu-items

--- src/drop-down-menu.js
+++ src/drop-down-menu.js
@@ -1,10 +1,9 @@
 export default function makeDropDown(menuRoot) {
-  const menuItems = menuRoot.querySelector('.menu-items');
   const menuTitle = menuRoot.querySelector('.menu-title');
   menuTitle.addEventListener('mouseenter', () => {
-    menuItems.classList.add('active');
+    menuRoot.classList.add('active');
   });
   menuRoot.addEventListener('mouseleave', () => {
-    menuItems.classList.remove('active');
+    menuRoot.classList.remove('active');
   });
 }
